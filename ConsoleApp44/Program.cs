94616c3dfb58408377b8aaf6d6999e2f9a6f9d5b
MEGA <MEGA@gmail.com>
2025-02-25 14:51:59 +0200

    Add project files.

---
+++ ConsoleApp44/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp44
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+            int diddy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Sort.SortN(array, diddy));
+        }
+    }
+}
---
+++ ConsoleApp44/Sort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp44
+{
+    internal class Sort
+    {
+        public static int SortN(int[] array, int target)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == target)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
+
+
+
